c707278b17af5d965e679b3534a2cbf05cf7a63b
Johan <johanmorenoqc@gmail.com>
2024-04-30 20:49:38 -0500

    se agrego el precio de los productos

--- pizzeria.py
+++ pizzeria.py
@@ -1,6 +1,11 @@
 print("Bienvenido a nuestra pizzeria")
 print("Menú")
-print("1. Hawaiana")
-print("2. Pepperoni")
-print("3. Napolitana")
-print("4. Champiñones")
+print("1. Hawaiana $10000")
+print("2. Pepperoni $20000")
+print("3. Napolitana $13500")
+print("4. Champiñones $14600")
+
+hawaiana = 10000
+pepperoni = 20000
+napolitana = 13500
+champiñones = 14600
